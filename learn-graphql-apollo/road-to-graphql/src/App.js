9d3eaf7354b0932ac2d54affd19359349732ffaf
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-06-04 12:39:55 +0530

    road to learn graphql

---
+++ learn-graphql-apollo/road-to-graphql/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+// Features:
+// - Star/Unstar a repository
+
+class App extends Component {
+  render() {
+    return <h2 className="app">GraphQL</h2>;
+  }
+}
+
+export default App;
