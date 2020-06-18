9ecfe42b470407015fc0c9baa6495cde6daf2d09
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-06-18 19:00:42 +0530

    Advance Graphql

---
+++ fem/advance graphql/README.md
@@ -0,0 +1,1 @@
+## Grpahql
--- graphql-road/c-react-apollo/src/App.js
+++ graphql-road/c-react-apollo/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Profile from "./Profile";
-import "./App.css";
+import React from 'react';
+import Profile from './Profile';
+import './App.css';
 
-function App() {
+function App({ props }) {
   return (
     <div className="App">
       <Profile />
